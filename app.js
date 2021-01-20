b3111f3b1f43651d3afad6df3ff13f02b2f55947
groria0330 <imai.prog330@gmail.com>
2021-01-21 08:34:34 +0900

    ルーティング

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const mysql = require('mysql')
+
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public'));  //静的ファイル(public)の読み込み
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs');
+});
+
+
+app.get('/signin', (req, res) => {
+    res.render('signin.ejs');
+});
+
+app.get('/inquiry', (req, res) => {
+    res.render('inquiry.ejs');
+});
+
+app.get('/kiyaku', (req, res) => {
+    res.render('kiyaku.ejs');
+});
+
+app.get('/pr', (req, res) => {
+    res.render('pr.ejs');
+});
+
+app.get('/application', (req, res) => {
+    res.render('application.ejs');
 });
 
 app.listen(3000);
